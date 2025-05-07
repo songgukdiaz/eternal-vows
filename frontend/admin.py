917427ffdbaaa95ca9fa274558e216a9db9e265c
Song Guk Diaz <songguksamueldiaz99@gmail.com>
2025-05-06 20:57:06 -0500

    added model to be controled by admin

--- frontend/admin.py
+++ frontend/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import InvitationCode
 # Register your models here.
+@admin.register(InvitationCode)
+class InvitationCodeAdmin(admin.ModelAdmin):
+    list_display = ('code', 'description', 'is_active', 'created_at')
+    list_filter = ('is_active', 'created_at')
+    search_fields = ('code', 'description')
+    ordering = ('code',)
