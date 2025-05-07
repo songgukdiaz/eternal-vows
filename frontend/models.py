097aa6d7e84d6a1c2bcd671e11b805562c99cf4a
Song Guk Diaz <songguksamueldiaz99@gmail.com>
2025-05-06 20:57:19 -0500

    added model for codes

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class InvitationCode(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=100, blank=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.code
